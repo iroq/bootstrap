9869176fa33bfe6c925c281d4ecf816c5c8fded0
Michal Szewczak <michaelszewczak@gmail.com>
2016-12-19 12:49:19 +0000

    update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
+# setup keys
+git clone https://github.com/iroq/keys $HOME/.keys &&
+ln -fs $HOME/keys/.git-credentials $HOME/.git-credentials
+
 # setup dots
 git clone --separate-git-dir=$HOME/.dots http://github.com/iroq/dots $HOME/dots-tmp &&
-rm -r ~/dots-tmp/ &&
-alias config='/usr/bin/git --git-dir=$HOME/.dots/ --work-tree=$HOME'
+rm -r $HOME/dots-tmp/ 
 
-# setup keys
-git clone https://github.com/iroq/keys ~/keys &&
-ln -fs ~/keys/.git-credentials ~/.git-credentials
