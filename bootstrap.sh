3bb3124e2fb29ae0c19078ea91aa86e27228b8c2
Michal Szewczak <michaelszewczak@gmail.com>
2016-12-19 14:29:00 +0100

    add checkout

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,4 +7,5 @@ ln -fs $HOME/keys/.git-credentials $HOME/.git-credentials
 # setup dots
 git clone --separate-git-dir=$HOME/.dots http://github.com/iroq/dots $HOME/dots-tmp &&
 rm -r $HOME/dots-tmp/ 
-
+alias config='/usr/bin/git --git-dir=$HOME/.dots/ --work-tree=$HOME'
+config checkout -- .
